21232bc44df4f1d9c19382620fd26eb0d598fb8e
kartikeyan misra <92641682+kartide@users.noreply.github.com>
2024-02-23 09:12:23 +0530

    Add files via upload

---
+++ question.py
@@ -0,0 +1,23 @@
+quiz_data = [
+    {
+        "question": "What is a database?",
+        "choices": ["Organized collection of information that cannot be accessed, updated, and managed", "Collection of data or information without organizing", "Organized collection of data or information that can be accessed, updated, and managed", "Organized collection of data that cannot be updated"],
+        "answer": "Organized collection of data or information that can be accessed, updated, and managed"
+    },
+    {
+        "question": "Which of the following is not a feature of DBMS?",
+        "choices": ["Minimum Duplication and Redundancy of Data", "High Level of Security", "Single-user Access only", "Support ACID Property"],
+        "answer": "Single-user Access only"
+    },
+    {
+        "question": "What is information about data called?",
+        "choices": ["Hyper data", "Tera data", "Meta data", "Relations"],
+        "answer": "Meta data"
+    },
+    {
+        "question": "What does an RDBMS consist of?",
+        "choices": ["Collection of Records", "Collection of Keys", "Collection of Tables", "Collection of Fields"],
+        "answer": "Collection of Tables"
+    }
+    # Add more questions here
+]
